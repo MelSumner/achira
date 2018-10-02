4303184a0f474236b7dd0a6a07043553fe444e2f
Melanie Sumner <melaniersumner@gmail.com>
2018-10-02 09:45:47 -0500

    refactor part 1, abstracted the responses

--- responses.js
+++ responses.js
@@ -1,5 +1,33 @@
-module.exports.EXPLETIVE_RESPONSE = "Oops! Looks like you used an expletive. Please note that swearing is against the terms and conditions of enrichment center.";
-module.exports.GUYS_RESPONSE = "Please consider more inclusive language so none of the companion cubes feel left out.";
-module.exports.HELLO_RESPONSE = "Hello and, again, welcome to the Aperture Science computer-aided enrichment center.";
-module.exports.THANK_RESPONSE = "Thank you for participating in this Aperture Science computer-aided enrichment activity.";
-module.exports.CAKE_RESPONSE = "Cake and grief counseling will be available at the conclusion of the test.";
+module.exports = {
+    MODERATOR_MESSAGES: [{
+        //review messages for swearing
+        "title": "EXPLETIVE",
+        "message": "Oops! Looks like you used an expletive. Please note that swearing is against the terms and conditions of enrichment center.",
+        "pattern": /regular expression literal in JS/
+        }, {
+        //encourage more inclusive language if someone says "guys"
+        "title": "GUYS",
+        "message": "",
+        "pattern": /regular expression literal/
+      }
+    ],
+  
+    BOT_MENTION_MESSAGES: [{
+        //if someone says hello @ the bot
+        "title": "HELLO",
+        "message": "",
+        "pattern": /regular expression literal/
+        }, {
+        // if someone thanks @ the bot
+        "title": "THANK",
+        "message": "",
+        "pattern": /regular expression literal/    
+        }, {
+        //if someone says something else @ the bot
+        "title": "CAKE",
+        "message": "",
+        "pattern": /regular expression literal/
+        }
+  
+    ]
+  }
